0346543b4d022fcf454fec1df5662c67580e48df
Troy Do <hoangdov@gmail.com>
2018-09-30 00:43:53 -0700

    Python array Intersection

--- arrInter.py
+++ arrInter.py
@@ -7,25 +7,25 @@ class Solution:
         """
         n1 = sorted(nums1)
         n2 = sorted(nums2)
-        intersection = [2,2]
+        intersection = []
         len1 = len(nums1)
         len2 = len(nums2)
         a,b = 0, 0
-        print(n1, "with length", len(nums1))
-        print(n2, "with length", len(nums2))
-        while a < len1 & b < len2:
+        # print(n1, "with length", len(nums1))
+        # print(n2, "with length", len(nums2))
+        while a < len1 and b < len2:
             if n1[a] < n2[b]:
-                print("increasing a")
+                # print("increasing a")
                 a +=1
             elif n2[b] < n1[a]:
-                print("increasing b")
+                # print("increasing b")
                 b +=1
             else:
-                print("adding value %s to intersecrtion array"%(n1[a]))
+                # print("adding value %s to intersecrtion array"%(n1[a]))
                 intersection.append(n1[a])
                 a +=1
                 b +=1
-            print(intersection)
+            # print(intersection)
         return intersection
                 
         
