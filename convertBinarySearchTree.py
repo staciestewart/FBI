526259c6860c64a1c57a74b30df6aa045b15a2c8
Troy Do <hoangdov@gmail.com>
2018-10-08 01:13:43 -0700

    Convert Binary Search Tree python

---
+++ convertBinarySearchTree.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+class Solution:
+    def init(self):
+        self.head = None
+        
+    def treeToDoublyList(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        def traverse(node):
+            if not node.left and not node.right:
+                return node, node
+            if node.left:
+                hl, tl = traverse(node.left)
+                tl.right = node
+                node.left = tl
+            else:
+                hl, tl = node, node
+            if node.right:
+                hr, tr = traverse(node.right)
+                hr.left = node
+                node.right = hr
+            else:
+                hr, tr = node, node
+            return hl, tr
+
+        if not root:
+            return None
+        hl, tr = traverse(root)
+        hl.left = tr
+        tr.right = hl
+        return hl
+        
+        
